bda01f60491a4acf8e1aaf2f097f7c758cac6a91
xiaoli <2585960098@qq.com>
2023-09-23 11:16:14 +0800

    first

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hhhhome</div>
+</template>
+
+<script>
+export default {
+  name: "My-Home",
+  components: {},
+};
+</script>
+
+<style>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+<<<<<<< HEAD
+  transpileDependencies: true
+=======
+  transpileDependencies: true,
+  // publicPath:'https://strive-cli.oss-cn-beijing.aliyuncs.com/aaa/'
+>>>>>>> 24f9a1543f6a8904e41697bdc9385d2e06fe7fb3
+})
